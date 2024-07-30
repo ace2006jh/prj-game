7b6d85369a63ed8f770b6c0bd02576daef971396
parkjh <ace2006jh@naver.com>
2024-07-30 16:40:20 +0900

    점수, 몹하나 추가, 목숨, 피격 추가

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -31,16 +31,25 @@ const Game = () => {
     let bullets;
     let enemies;
     let specialEnemies;
+    let specialEnemies2;
     let enemyBullets;
     let bossBullets;
     let lastFired = 0;
     let boss;
+    let lives;
+    let livesGroup;
+    let playerImmune;
+    let playerExploding;
+    let score = 0;
+    let scoreText;
 
+    const initialLives = 3;
+    const respawnDelay = 2000;
     const fireRate = 100;
     const enemySpeed = 100;
     const specialEnemySpeed = 100;
-    const specialEnemyHealth = 10;
-    const BossHealth = 24;
+    const specialEnemyHealth = 6;
+    const BossHealth = 18;
     const bossBulletSpeed = 200;
     const bossFireRate = 500; // 보스 총알 발사 주기 (ms)
     let background;
@@ -51,9 +60,14 @@ const Game = () => {
       this.load.image("bullet", "/bullet.png"); // 총알 이미지 경로
       this.load.image("enemyy", "/enemy.png"); // 적 이미지 경로
       this.load.image("specialEnemy", "/specialEnemy.png"); // 새로운 적 이미지 경로
+      this.load.image("specialEnemy2", "/specialEnemy2.png"); // 새로운 적 이미지 경로
       this.load.image("enemyBullet", "/enemyBullet.png"); // 적의 탄환 이미지 경로
       this.load.image("Boss", "/boss.png"); // 보스 이미지 경로
       this.load.image("bossBullet", "/bossBullet.png"); // 보스의 탄환 이미지 경로
+      this.load.spritesheet("boom", "/boom.png", {
+        frameWidth: 32,
+        frameHeight: 32,
+      });
       this.load.spritesheet("bossExplosion", "/bossExplosion.png", {
         frameWidth: 128,
         frameHeight: 128,
@@ -86,16 +100,38 @@ const Game = () => {
         classType: Phaser.Physics.Arcade.Image,
       });
 
+      livesGroup = this.add.group({
+        key: "player",
+        repeat: initialLives - 1,
+        setXY: { x: 550, y: 50, stepX: -30 },
+      });
+
+      scoreText = this.add.text(16, 16, "Score: 0", {
+        fontSize: "32px",
+        fill: "#fff",
+      });
+
+      lives = initialLives;
+      playerImmune = false;
+      playerExploding = false;
+
       enemies = this.physics.add.group({
         defaultKey: "enemyy",
-        maxSize: 20,
+        maxSize: 30,
         classType: Phaser.Physics.Arcade.Image,
         runChildUpdate: true,
       });
 
       specialEnemies = this.physics.add.group({
         defaultKey: "specialEnemy",
-        maxSize: 5,
+        maxSize: 8,
+        classType: Phaser.Physics.Arcade.Image,
+        runChildUpdate: true,
+      });
+
+      specialEnemies2 = this.physics.add.group({
+        defaultKey: "specialEnemy2",
+        maxSize: 4,
         classType: Phaser.Physics.Arcade.Image,
         runChildUpdate: true,
       });
@@ -146,15 +182,22 @@ const Game = () => {
 
       // 보스 생성
       this.time.addEvent({
-        delay: 20000,
+        delay: 22000,
         callback: createBoss,
         callbackScope: this,
       });
 
       // 새로운 적을 3000ms마다 생성하는 이벤트 추가
       this.time.addEvent({
-        delay: 8000,
+        delay: 6000,
         callback: createSpecialEnemy,
+        callbackScope: this,
+        loop: true,
+      });
+
+      this.time.addEvent({
+        delay: 13000,
+        callback: createSpecialEnemy2,
         callbackScope: this,
         loop: true,
       });
@@ -163,6 +206,13 @@ const Game = () => {
       this.physics.add.collider(
         bullets,
         specialEnemies,
+        damageSpecialEnemy,
+        null,
+        this,
+      );
+      this.physics.add.collider(
+        bullets,
+        specialEnemies2,
         damageSpecialEnemy,
         null,
         this,
@@ -181,25 +231,37 @@ const Game = () => {
         repeat: 0,
         hideOnComplete: true,
       });
+
+      this.anims.create({
+        key: "boom",
+        frames: this.anims.generateFrameNumbers("boom", {
+          start: 0,
+          end: 9,
+        }),
+        frameRate: 16,
+        repeat: 0,
+        hideOnComplete: true,
+      });
     }
 
     function update(time, delta) {
       background.tilePositionY -= 1;
+      if (!playerExploding) {
+        if (cursors.left.isDown) {
+          player.setVelocityX(-200);
+        } else if (cursors.right.isDown) {
+          player.setVelocityX(200);
+        } else {
+          player.setVelocityX(0);
+        }
 
-      if (cursors.left.isDown) {
-        player.setVelocityX(-200);
-      } else if (cursors.right.isDown) {
-        player.setVelocityX(200);
-      } else {
-        player.setVelocityX(0);
-      }
-
-      if (cursors.up.isDown) {
-        player.setVelocityY(-200);
-      } else if (cursors.down.isDown) {
-        player.setVelocityY(200);
-      } else {
-        player.setVelocityY(0);
+        if (cursors.up.isDown) {
+          player.setVelocityY(-200);
+        } else if (cursors.down.isDown) {
+          player.setVelocityY(200);
+        } else {
+          player.setVelocityY(0);
+        }
       }
 
       // 적을 아래로 이동시키기 위해 update에서 처리
@@ -220,6 +282,20 @@ const Game = () => {
             }
           } else {
             specialEnemy.setVelocityY(specialEnemySpeed);
+          }
+        }
+      });
+
+      specialEnemies2.children.iterate((specialEnemy2) => {
+        if (specialEnemy2 && specialEnemy2.active) {
+          if (specialEnemy2.y >= 260) {
+            specialEnemy2.setVelocityY(0);
+            if (time > specialEnemy2.lastFired + 1000) {
+              fireBossBullet(specialEnemy2);
+              specialEnemy2.lastFired = time;
+            }
+          } else {
+            specialEnemy2.setVelocityY(specialEnemySpeed);
           }
         }
       });
@@ -287,6 +363,26 @@ const Game = () => {
       }
     }
 
+    function createSpecialEnemy2() {
+      const specialEnemy = specialEnemies2.create(150, 50, "specialEnemy2");
+      const specialEnemy2 = specialEnemies2.create(450, 50, "specialEnemy2");
+
+      if (specialEnemy) {
+        specialEnemy.health = specialEnemyHealth;
+        specialEnemy.lastFired = 0;
+        specialEnemy.setVelocityY(specialEnemySpeed);
+        specialEnemy.setCollideWorldBounds(true);
+        specialEnemy.body.onWorldBounds = true;
+      }
+      if (specialEnemy2) {
+        specialEnemy2.health = specialEnemyHealth;
+        specialEnemy2.lastFired = 0;
+        specialEnemy2.setVelocityY(specialEnemySpeed);
+        specialEnemy2.setCollideWorldBounds(true);
+        specialEnemy2.body.onWorldBounds = true;
+      }
+    }
+
     function fireEnemyBullet(enemy) {
       const bullet = enemyBullets.getFirstDead(false);
       if (bullet) {
@@ -327,7 +423,12 @@ const Game = () => {
       bullet.disableBody(true, true);
       specialEnemy.health -= 1;
       if (specialEnemy.health <= 0) {
+        const explosion = this.add
+          .sprite(specialEnemy.x, specialEnemy.y, "bossExplosion")
+          .play("bossExplosion");
+
         specialEnemy.disableBody(true, true);
+        updateScore(500);
       }
     }
 
@@ -336,6 +437,7 @@ const Game = () => {
       boss.health -= 1;
       if (boss.health <= 0) {
         boss.disableBody(true, true);
+        updateScore(2500);
         boss.fireTimer.remove(); // 보스가 파괴되면 총알 발사 타이머 제거
 
         // 보스 파괴 애니메이션 실행
@@ -347,12 +449,58 @@ const Game = () => {
 
     function destroyEnemy(bullet, enemy) {
       bullet.disableBody(true, true);
+      const boomm = this.add.sprite(enemy.x, enemy.y, "boom").play("boom");
       enemy.disableBody(true, true);
+      updateScore(100);
     }
 
     function playerHit(player, bullet) {
+      if (playerImmune || playerExploding) return;
+
       bullet.disableBody(true, true);
-      // 플레이어가 적의 탄환에 맞았을 때의 처리 로직 추가
+      lives--;
+
+      if (livesGroup.getChildren().length > 0) {
+        livesGroup.getChildren()[lives].destroy();
+      }
+
+      playerExploding = true;
+
+      // 플레이어 파괴 애니메이션 실행
+      const explosion = this.add.sprite(player.x, player.y, "boom");
+      explosion.play("boom");
+
+      // 애니메이션이 완료된 후 플레이어 비활성화 및 재생성
+      explosion.on("animationcomplete", () => {
+        explosion.destroy();
+        player.disableBody(true, true);
+        playerExploding = false;
+        if (lives > 0) {
+          respawnPlayer.call(this);
+        } else {
+          // 게임 오버 처리 로직 추가 가능
+        }
+      });
+    }
+
+    function respawnPlayer() {
+      playerImmune = true;
+      player.enableBody(true, 300, 700, true, true);
+      player.alpha = 0.5; // 투명도를 조절하여 무적 상태 표시
+
+      this.time.addEvent({
+        delay: respawnDelay,
+        callback: () => {
+          playerImmune = false;
+          player.alpha = 1;
+        },
+        callbackScope: this,
+      });
+    }
+
+    function updateScore(points) {
+      score += points;
+      scoreText.setText("Score: " + score);
     }
 
     return () => {
